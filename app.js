3c60de8e38b8eb39afdd7086ea409c04013761c8
unknown <ecemnurbalta@gmail.com>
2021-12-27 16:59:59 +0300

    login-register

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
+// Connect DB
+mongoose.connect('mongodb://localhost/smartedu-db').then(() => {
+  console.log('DB Connected Successfully');
+});
 
 // Template Engine
 app.set('view engine', 'ejs');
 
 // Middlewares
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
 const port = 3000;
 app.listen(port, () => {
